144104eb821181e4e7728d788b1a9bed2962dd93
nqbao47 <baonguyengau14@gmail.com>
2023-06-16 09:35:07 +0700

    Modified ContentTast & add .eslintrc

--- cypress/e2e/ContentTest.cy.js
+++ cypress/e2e/ContentTest.cy.js
@@ -1,26 +1,21 @@
 describe("Content Component", () => {
   beforeEach(() => {
-        
-        if (Cypress.mocha.getRunner().suite.ctx.currentTest.title !== "should delete a task") {
-          cy.clearLocalStorage();
-        }
+      cy.clearLocalStorage();
+      const tasks = [
+        { id: 1, content: "Task 1", status: "Completed" },
+        { id: 2, content: "Task 2", status: "Urgent" },
+        { id: 3, content: "Task 3", status: "Later" },
+      ];
+
+      // Storage tasks to LS
+      cy.window().then((win) => {
+        win.localStorage.setItem("tasks", JSON.stringify(tasks));
+      });
+    
   });
 
   it("should render tasks correctly", () => {
-    // Kiểm tra hiển thị
     cy.visit("http://localhost:3000/");
-
-    // Tạo các task mẫu
-    const tasks = [
-      { id: 1, content: "Task 1", status: "Completed" },
-      { id: 2, content: "Task 2", status: "Urgent" },
-      { id: 3, content: "Task 3", status: "Later" },
-    ];
-
-    // Lưu tasks vào Local Storage
-    cy.window().then((win) => {
-      win.localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
 
     // Kiểm tra xem trạng thái của task có id là 1 đã hiển thị đúng hay không
     cy.get(".task-item")
@@ -31,17 +26,6 @@ describe("Content Component", () => {
   });
 
   it("should edit a task", () => {
-    // Lưu task mẫu vào Local Storage
-    const tasks = [
-      { id: 1, content: "Task 1", status: "Completed" },
-      { id: 2, content: "Task 2", status: "Urgent" },
-      { id: 3, content: "Task 3", status: "Later" },
-    ];
-    cy.window().then((win) => {
-      win.localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
-
-    // Kiểm tra hiển thị
     cy.visit("http://localhost:3000/");
 
     // Chỉnh sửa trạng thái của task có id là 1 thành "Later"
@@ -62,18 +46,7 @@ describe("Content Component", () => {
   });
 
   it("should delete a task", () => {
-    // Kiểm tra hiển thị
-    cy.visit("http://localhost:3000/", {
-      onBeforeLoad: (win) => {
-        // Lưu trữ laih
-        win.localStorage.setItem("tasks", JSON.stringify([
-          { id: 1, content: "Task 1", status: "Completed" },
-          { id: 2, content: "Task 2", status: "Urgent" },
-          { id: 3, content: "Task 3", status: "Later" },
-        ]));
-      },
-    });
-
+    cy.visit("http://localhost:3000/");
     // Kiểm tra xem có 3 task-item được hiển thị ban đầu hay không
     cy.get(".task-item").should("have.length", 3);
 
